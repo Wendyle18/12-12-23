5ab942cd276815ffcb20db044d1e7a1cb1c3f52d
Wendyle Christian M. Seno <106366417+Wendyle18@users.noreply.github.com>
2023-12-12 21:58:49 +0800

    Add files via upload

---
+++ web/pages/403.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href= "../assets/css/404.css">
+    <title>Error 403</title>
+</head>
+<body>
+    <a href = "<?= BASE_URL ?>dashboard">← Go Back to dashboard </a>
+    <h1>Error 403</h1>
+    <p>Forbidden</p>
+</body>
+</html>
